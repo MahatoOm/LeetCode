0a2751b586b126f2fc92d0614b0005062d1af461
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2024-12-30 06:01:33 -0600

    Time: 0 ms, Memory: 19.3 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -7,41 +7,42 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         
-#         def max_depth(node, depth,answer):
-#             if not node:
-#                 return
-            
-#             if not node.left and not node.right:
-#                 answer = max(answer,depth)
-                
-#             # if node.left:
-                
-#             max_depth(node.left, depth + 1,answer)
-#             # if node.right:
-#             #     answer = max(answer, depth +1)
-                
-#             max_depth(node.right, depth +1,answer)
-            
-            
-            
-            
-#         depth = 1
-#         answer = 0
-       
-#         if root:
-#             max_depth(root, depth,answer)
-#         return answer
-
-
-        def buttom_up(node):
+        def top_down(node, depth):
+            nonlocal max_depth
             if not node:
-                return 0
-            left_param = buttom_up(node.left)
-            right_param = buttom_up(node.right)
+                return
             
-            return max(left_param , right_param) + 1
+            if not node.left and not node.right:
+                max_depth = max(max_depth ,depth)
+                
+            # if node.left:
+                
+            top_down(node.left, depth + 1)
+            # if node.right:
+            #     answer = max(answer, depth +1)
+                
+            top_down(node.right, depth +1)
+            
+            
+            
+            
+        max_depth = 0 
         
-        return buttom_up(root)
+       
+        if root:
+            top_down(root, 1)
+        return max_depth
+
+
+#         def buttom_up(node):
+#             if not node:
+#                 return 0
+#             left_param = buttom_up(node.left)
+#             right_param = buttom_up(node.right)
+            
+#             return max(left_param , right_param) + 1
+        
+#         return buttom_up(root)
             
         
     
