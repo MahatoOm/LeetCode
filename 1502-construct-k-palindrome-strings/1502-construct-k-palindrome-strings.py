97c1ef777eaac771f0d04fb2e099487ad8f6ab6f
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-10 18:58:52 -0600

    Time: 23 ms (98.24%), Space: 18 MB (34.36%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        
+        # creating hash table in ordert to store the count of character
+        if len(s) < k:
+            return False
+        odd_count = 0
+        # char_dict = {}
+        for char in set(s):
+            char_count = s.count(char)
+            if char_count %2 ==1:
+                odd_count += 1
+            # char_dict[char] = char_count
+
+        if odd_count > k:
+            return False
+        else:
+            return True
