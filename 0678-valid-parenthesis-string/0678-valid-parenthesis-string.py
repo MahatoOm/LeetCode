f78be63975359d5ff20ee6cdc77a90cab5d906a3
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-12 06:18:20 -0600

    Time: 0 ms (100%), Space: 17.7 MB (27%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -25,25 +25,23 @@ class Solution:
                     print("cobra")
                     return False
 
-        open_count = 0
+        close_count = 0
         star_count = 0
         for i in range(n-1, -1, -1):
             if s[i] == ")":
-                open_count += 1
+                close_count += 1
             elif s[i] == "(":
-                open_count -=1
+                close_count -=1
             else:
                 star_count +=1
 
 
-            if open_count < 0:
+            if close_count < 0:
                 if star_count > 0:
                     star_count -= 1
-                    open_count += 1
+                    close_count += 1
                 else:
-
-                    print("cobra")
                     return False
-        print("cobra")
+     
         
         return  True
