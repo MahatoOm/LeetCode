4fc46a7b578bcd26b1b51f36ab4e7543af411eed
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-12 02:56:24 -0600

    Time: 0 ms, Memory: 18.2 MB - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,6 +1,31 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         
+        
+        # with binary template 2 where each iteration right is mid
+#         l = 0
+#         r = len(nums) -1
+#         if r == 0:
+#             return r
+#         if r ==1:
+#             return r if nums[r] > nums[0] else 0
+        
+#         while l < r:
+            
+#             m = (l+ r) //2
+            
+#             if nums[m-1]< nums[m] > nums[m+1]:
+#                 return m
+            
+#             if  nums[m-1]< nums[m]< nums[m+1]:
+#                 l = m + 1
+#             else:
+#                 r = m 
+                
+                
+#         return r
+
+        # with template 3 
         l = 0
         r = len(nums) -1
         if r == 0:
@@ -8,7 +33,7 @@ class Solution:
         if r ==1:
             return r if nums[r] > nums[0] else 0
         
-        while l < r:
+        while l + 1 < r:
             
             m = (l+ r) //2
             
@@ -16,11 +41,11 @@ class Solution:
                 return m
             
             if  nums[m-1]< nums[m]< nums[m+1]:
-                l = m + 1
+                l = m 
             else:
                 r = m 
                 
                 
-        return r
+        return l if nums[l] > nums[r] else r
             
                 
