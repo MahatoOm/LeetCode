06c1c7b578b053aa5b08c05726a3354a81a58910
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-12 06:16:20 -0600

    Time: 0 ms (100%), Space: 17.7 MB (27%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,49 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        
+        n = len(s)
+        if n == 1  and not (s[0] == "*"):
+            return False 
+        
+        open_count = 0
+        star_count = 0
+        for i in range(n):
+            if s[i] == "(":
+                open_count += 1
+            elif s[i] == ")":
+                open_count -=1
+            else:
+                star_count +=1
+
+
+            if open_count < 0:
+                if star_count > 0:
+                    star_count -= 1
+                    open_count += 1
+                else:
+
+                    print("cobra")
+                    return False
+
+        open_count = 0
+        star_count = 0
+        for i in range(n-1, -1, -1):
+            if s[i] == ")":
+                open_count += 1
+            elif s[i] == "(":
+                open_count -=1
+            else:
+                star_count +=1
+
+
+            if open_count < 0:
+                if star_count > 0:
+                    star_count -= 1
+                    open_count += 1
+                else:
+
+                    print("cobra")
+                    return False
+        print("cobra")
+        
+        return  True
