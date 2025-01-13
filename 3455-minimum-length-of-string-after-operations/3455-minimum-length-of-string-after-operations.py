be7b4a73d8cfe18ca3d65d9fd2fd110d3e475ae9
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-12 21:04:42 -0600

    Time: 99 ms (73.33%), Space: 18.9 MB (32.7%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.py
@@ -2,11 +2,7 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         
         # only frequency matters no need to actually delete the element
-        # store = list(s)
-
-
-        n = len(s)
-        # char_count = [] 
+        n = len(s) 
         for char in set(s):
             count = s.count(char)
             if count >= 3:
@@ -15,11 +11,4 @@ class Solution:
                 else:
                     n = n - count + 2 
 
-                
-                # left , right = s.find(char) , s.rfind(char)
-                # store[left] = -1
-                # store[right] = -1
-        # if -1 in store:
-        #     store = [char for char in store if char != -1]
-            
         return n
