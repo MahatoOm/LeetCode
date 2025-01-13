4800f3072ac954ed67fb8d6ecaa34ef7a5b8cd40
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-13 03:38:25 -0600

    Time: 103 ms (21.93%), Space: 28.7 MB (37.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,20 +1,30 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        frquency_count = defaultdict(int)
-        for _, element in enumerate(nums):
-            frquency_count[element] += 1 
+        # frquency_count = defaultdict(int)
+        # for _, element in enumerate(nums):
+        #     frquency_count[element] += 1 
             
-        left = 0
-        right = k
+        # left = 0
+        # right = k
 
-        while left < k:
+        # while left < k:
 
-            count = max(frquency_count.keys())
+        #     count = max(frquency_count.keys())
             
-            if frquency_count[count] < k:
-                k -= frquency_count[count]
-                del frquency_count[count]
-            else:
-                return count
+        #     if frquency_count[count] < k:
+        #         k -= frquency_count[count]
+        #         del frquency_count[count]
+        #     else:
+        #         return count
+
+
+# using natural property of heap, minheap as lowest element in front
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
 
