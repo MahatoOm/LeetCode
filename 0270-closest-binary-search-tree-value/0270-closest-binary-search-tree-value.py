b6680e019323f2da3c0b06123891338e95dfdbc9
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-01-16 19:48:29 -0600

    Time: 0 ms (100%), Space: 19.7 MB (18.12%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.py
@@ -7,42 +7,69 @@
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
 
-        
-        return self.bfs(root, target)
-        
-    def bfs(self, root, target):
-        if not root:
-            return
-        
-        queue = collections.deque()
-        queue.append(root)
-        minimal_val = 10**9
+        node = root
+        minimal_val = 10 ** 9
         output = 10 ** 9
         
-        while queue:
-            
-            size = len(queue)
-            
-            for i in range(size):
-                node = queue.popleft()
-                
-                if abs(node.val - target) == minimal_val:
-                    if output > node.val:
-                        output = node.val
-
-                if abs(node.val - target) < minimal_val:
-
-                    minimal_val = abs(node.val - target)    
+        while node:
+            if abs(target - node.val) == minimal_val:
+                if output > node.val:
                     output = node.val
-
-
-                if node.left:
-                    queue.append(node.left)
-                    
-                if node.right:
-                    queue.append(node.right)
-                    
+            
+            if abs(target - node.val) < minimal_val:
+                minimal_val = abs(target - node.val)
+                output = node.val
+            
+            if target > node.val:
+                node = node.right
+                
+            elif target <= node.val:
+                node = node.left
+                
+                
         return output
+        
+        
+        
+        
+        
+        
+        
+#         return self.bfs(root, target)
+        
+#     def bfs(self, root, target):
+#         if not root:
+#             return
+        
+#         queue = collections.deque()
+#         queue.append(root)
+#         minimal_val = 10**9
+#         output = 10 ** 9
+        
+#         while queue:
+            
+#             size = len(queue)
+            
+#             for i in range(size):
+#                 node = queue.popleft()
+                
+#                 if abs(node.val - target) == minimal_val:
+#                     if output > node.val:
+#                         output = node.val
+
+#                 if abs(node.val - target) < minimal_val:
+
+#                     minimal_val = abs(node.val - target)    
+#                     output = node.val
+
+
+#                 if node.left:
+#                     queue.append(node.left)
+                    
+#                 if node.right:
+#                     queue.append(node.right)
+                    
+#         return output
                     
                     
                     
