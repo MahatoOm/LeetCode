2a7933876ca5f7c263302b6092a4bdb54635efac
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-02-02 09:51:57 -0600

    Time: 20 ms (87.78%), Space: 30.1 MB (43.68%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,36 +1,36 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         
-        # left = 0
-        # right = len(nums) -1
-        # check = []
-        # while left <= right:
+        left = 0
+        right = len(nums) 
+        check = [0] * right
+        while left < right:
 
-        #     if nums[left] in check:
-        #         return nums[left]
-        #     check.append(nums[left])
+            if check[nums[left]] != 0:
+                return nums[left]
+            check[nums[left]] = nums[left]
             
-        #     left+=1
+            left+=1
 
 
 
         # 'low' and 'high' represent the range of values of the target
-        low = 1
-        high = len(nums) - 1
+        # low = 1
+        # high = len(nums) - 1
         
-        while low <= high:
-            cur = (low + high) // 2
-            count = 0
+        # while low <= high:
+        #     cur = (low + high) // 2
+        #     count = 0
 
-            # Count how many numbers are less than or equal to 'cur'
-            count = sum(num <= cur for num in nums)
-            if count > cur:
-                duplicate = cur
-                high = cur - 1
-            else:
-                low = cur + 1
+        #     # Count how many numbers are less than or equal to 'cur'
+        #     count = sum(num <= cur for num in nums)
+        #     if count > cur:
+        #         duplicate = cur
+        #         high = cur - 1
+        #     else:
+        #         low = cur + 1
                 
-        return duplicate
+        # return duplicate
         
             
             
