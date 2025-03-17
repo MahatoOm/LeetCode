f2e3aa51931421f8792fcbe0445b9942f190c0bc
Om <117744764+Ommmmmmmmmm@users.noreply.github.com>
2025-03-16 22:54:36 -0500

    Time: 3 ms (70.86%), Space: 18.1 MB (24.91%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -3,11 +3,9 @@ class Solution:
         
         count = Counter(nums)
         # print(count)
-        odd_count = 0
+        
         for times in count.values():
             if times % 2 !=0:
-                odd_count += 1
-
-            if odd_count > 1:
-                return False 
+                return False
+                
         return True
