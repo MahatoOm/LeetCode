32fbd4ce8924a7c681ec72af70e68ad9a1f4d6a5
Om Mahato <117744764+MahatoOm@users.noreply.github.com>
2025-04-17 20:27:42 -0500

    Time: 16 ms (5.93%), Space: 18.9 MB (88.82%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,6 +1,36 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         
+    # Second try using BFS
+        def BFS(i):
+
+            queue = deque()
+            queue.append(i)
+            while queue:
+                node = queue.popleft()
+                visited.add(node)
+
+                for neighbor in Adjacent_graph[node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+
+
+        visited = set()
+        Adjacent_graph = defaultdict(list)
+        for i,j in edges:
+            Adjacent_graph[i].append(j)
+            Adjacent_graph[j].append(i)
+        print(Adjacent_graph)
+
+        result = 0
+        for i in range(n):
+            if i not in visited:
+                result += 1
+                BFS(i)
+
+        return result
+
+'''
         # First try with DFS,
         def DFS(i):
 
@@ -23,3 +53,4 @@ class Solution:
                 DFS(idx)
 
         return result
+'''
